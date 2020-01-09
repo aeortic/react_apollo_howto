cef2595887a83760f37457caac3f40403e6a891b
Chris Okerberg <seminative@gmail.com>
2020-01-09 13:17:25 -0500

    Added a form to create links

---
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import {useMutation} from '@apollo/react-hooks';
+
+const POST_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
+    post(description: $description, url: $url) {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
+
+export default function CreateLink (){
+
+  const [post, {data}] = useMutation(POST_MUTATION);
+  let descriptionInput;
+  let urlInput;
+  return (
+    <>
+      <form className="flex flex-column mt3"
+        onSubmit={e => {
+          e.preventDefault();
+          post({variables: {url: urlInput.value, description: descriptionInput.value}});
+          urlInput.value = '';
+          descriptionInput.value = '';
+        }}
+      >
+        <input
+          className="mb2"
+          ref={node => {
+            descriptionInput = node;
+          }}
+          type="text"
+          placeholder="A description for the link "
+        />
+        <input
+          className="mb2"
+          ref={node => {
+            urlInput = node;
+          }}
+          type="text"
+          placeholder="The URL for the link"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  ) 
+}
