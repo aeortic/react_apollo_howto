ac496ef8940db4e764f2f6edca2eb5dd571e769a
Chris Okerberg <seminative@gmail.com>
2020-01-13 16:15:47 -0500

    Added routing

--- hackernews-react-apollo/src/components/CreateLink.js
+++ hackernews-react-apollo/src/components/CreateLink.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import gql from 'graphql-tag'
 import {useMutation} from '@apollo/react-hooks';
+import {useHistory} from "react-router-dom";
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -15,15 +16,22 @@ const POST_MUTATION = gql`
 
 export default function CreateLink (){
 
-  const [post, {data}] = useMutation(POST_MUTATION);
+  const [post, {data}] = useMutation(POST_MUTATION,
+    {
+      onCompleted() { history.push('/') }
+    }
+  );
   let descriptionInput;
   let urlInput;
+  let history = useHistory();
   return (
     <>
       <form className="flex flex-column mt3"
         onSubmit={e => {
           e.preventDefault();
-          post({variables: {url: urlInput.value, description: descriptionInput.value}});
+          post({
+            variables: {url: urlInput.value, description: descriptionInput.value},
+          });
           urlInput.value = '';
           descriptionInput.value = '';
         }}
