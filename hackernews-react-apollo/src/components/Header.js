056801f99af996ed42e7de743693fa980e7f9e4e
Chris Okerberg <seminative@gmail.com>
2020-02-11 20:36:43 -0500

    Added authentication token via middleware

--- hackernews-react-apollo/src/components/Header.js
+++ hackernews-react-apollo/src/components/Header.js
@@ -2,9 +2,11 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {withRouter} from 'react-router'
 import {AUTH_TOKEN} from '../constants'
+import {useHistory} from "react-router-dom"
 
 export function Header() {
   const authToken = localStorage.getItem(AUTH_TOKEN)
+  const history = useHistory();
   return (
     <div className="flex pa1 justify-between nowrap orange">
       <div className="flex flex-fixed black">
@@ -22,9 +24,21 @@ export function Header() {
         )}
       </div>
       <div className="flex flex-fixed">
-        <Link to="/login" className="ml1 no-underline black">
-          login
-        </Link>
+        {authToken ? (
+          <div
+            className="ml1 pointer black"
+            onClick={() => {
+              localStorage.removeItem(AUTH_TOKEN)
+              history.push('/')
+            }}
+          >
+            logout
+          </div>
+          ) : (
+            <Link to="/login" className="ml1 no-underline black">
+              login
+            </Link>
+          )}
       </div>
     </div>
   )
