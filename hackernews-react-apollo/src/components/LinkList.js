27c1261fff475d5e488af4271236ec8edb37c31b
Chris Okerberg <seminative@gmail.com>
2020-02-18 10:55:26 -0500

    Added subscription for links and votes

--- hackernews-react-apollo/src/components/LinkList.js
+++ hackernews-react-apollo/src/components/LinkList.js
@@ -25,11 +25,86 @@ const FEED_QUERY = gql`
     }
   }
 `
+
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
+const subscribeToNewLinks = subscribeToMore => {
+  subscribeToMore({
+    document: NEW_LINKS_SUBSCRIPTION,
+    updateQuery: (prev, {subscriptionData}) => {
+      if (!subscriptionData.data) return prev
+      const newLink = subscriptionData.data.newLink
+      const exists = prev.feed.links.find(({id}) => id === newLink.id);
+      if (exists) return prev;
+
+      return Object.assign({}, prev, {
+        feed: {
+          links: [newLink, ...prev.feed.links],
+          count: prev.feed.links.length + 1,
+          __typename: prev.feed.__typename
+        }
+      })
+    }
+  })
+}
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+const subscribeToNewVotes = subscribeToMore => {
+  subscribeToMore({
+    document: NEW_VOTES_SUBSCRIPTION
+  })
+}
+
 export default function LinkList() {
-  const {loading, error, data} = useQuery(FEED_QUERY); 
+  const {loading, error, data, subscribeToMore} = useQuery(FEED_QUERY); 
 
   if (loading) return <div>Fetching</div>
   if (error) return <div>Error</div>
+
+  subscribeToNewLinks(subscribeToMore)
+  subscribeToNewVotes(subscribeToMore)
 
   const linksToRender = data.feed.links
 
